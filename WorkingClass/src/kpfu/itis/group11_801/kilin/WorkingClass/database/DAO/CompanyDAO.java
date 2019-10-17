821b600d687703b67cbc02e674235b09d2c3aaeb
Mikhail Kilin <1kilinmv1@gmail.com>
2019-10-17 14:34:59 +0300

    Changes in CompanyDAO

--- WorkingClass/src/kpfu/itis/group11_801/kilin/WorkingClass/database/DAO/CompanyDAO.java
+++ WorkingClass/src/kpfu/itis/group11_801/kilin/WorkingClass/database/DAO/CompanyDAO.java
@@ -1,14 +1,13 @@
-package kpfu.itis.group11_801.kilin.WorkingClass.database.DAO;
+package kpfu.itis.group11_801.kilin.workingClass.database.DAO;
 
-import kpfu.itis.group11_801.kilin.WorkingClass.database.Company;
-import kpfu.itis.group11_801.kilin.WorkingClass.database.User;
+import kpfu.itis.group11_801.kilin.workingClass.database.Company;
+import kpfu.itis.group11_801.kilin.workingClass.database.Image;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 public class CompanyDAO extends DAO<Company> {
@@ -48,9 +47,22 @@ public class CompanyDAO extends DAO<Company> {
 
     @Override
     public Company getById(int id) {
+        if (id == 0) {return null;}
         try {
             Statement statement = connection.createStatement();
             ResultSet rs = statement.executeQuery("SELECT * FROM public.company WHERE id=" + id + ";");
+            rs.next();
+            return getCompanyByResultSet(rs);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public Company getByName(String name) {
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM public.company WHERE name='" + name + "';");
             rs.next();
             return getCompanyByResultSet(rs);
         } catch (SQLException e) {
@@ -65,6 +77,8 @@ public class CompanyDAO extends DAO<Company> {
         try {
             Statement statement = connection.createStatement();
             statement.executeQuery("UPDATE public.company SET "
+                    + "name='" + elem.getName() + "', "
+                    + "photo_path='" + elem.getImage().getImagePath() + "', "
                     + "info='" + elem.getInfo() + "' WHERE id=" + id + ";"
             );
         } catch (SQLException e) {
@@ -90,15 +104,16 @@ public class CompanyDAO extends DAO<Company> {
 
     @Override
     public Company create(Company elem) {
+        String name = elem.getName();
         String info = elem.getInfo();
+        String photo = elem.getImage().getImagePath();
         try {
             Statement statement = connection.createStatement();
-            statement.executeQuery("INSERT INTO public.company " +
-                    "(info) "
+            ResultSet rs = statement.executeQuery("INSERT INTO public.company " +
+                    "(name, info, photo_path) "
                     + "VALUES "
-                    + "('" + info + "');"
+                    + "('" + name + "', '" + info + "', '" + photo + "') RETURNING id;"
             );
-            ResultSet rs = statement.getGeneratedKeys();
             rs.next();
             return getById(rs.getInt("id"));
         } catch (SQLException e) {
@@ -109,7 +124,16 @@ public class CompanyDAO extends DAO<Company> {
 
     private Company getCompanyByResultSet(ResultSet rs) throws SQLException {
         int id = rs.getInt("id");
+        String name = rs.getString("name");
         String info = rs.getString("info");
-        return new Company(id, info);
+        String imagePath = rs.getString("photo_path");
+        Image image = new Image(imagePath);
+        return new Company(id, name, info, image);
+    }
+
+    public static void main(String [] args) {
+        CompanyDAO dao = CompanyDAO.getCompanyDAO();
+        Company company = dao.create(new Company(0, "Name", "info", new Image("path")));
+        System.out.println(company.getId());
     }
 }
