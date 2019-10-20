f95b6d8b15c8aeb5f61ce2a20c685fa45039cabc
Mikhail Kilin <1kilinmv1@gmail.com>
2019-10-20 18:11:52 +0300

    Added All services for User

---
+++ WorkingClass/src/kpfu/itis/group11_801/kilin/WorkingClass/database/services/UserService.java
@@ -0,0 +1,72 @@
+package kpfu.itis.group11_801.kilin.workingClass.database.services;
+
+import kpfu.itis.group11_801.kilin.workingClass.database.DAO.UserDAO;
+import kpfu.itis.group11_801.kilin.workingClass.database.User;
+
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+import java.security.MessageDigest;
+
+public class UserService {
+    public User getByEmail(String email) {
+        UserDAO userDAO = UserDAO.getUserDAO();
+        return userDAO.getByEmail(email);
+    }
+
+    public User registrate(User u) {
+        UserDAO userDAO = UserDAO.getUserDAO();
+        if (getByEmail(u.getLogin()) != null || u.getDate().getYear() > 2001) {return null;}
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+            messageDigest.update(u.getPassword().getBytes());
+            String result = new String(messageDigest.digest());
+            u.setPassword(result);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return userDAO.create(u);
+    }
+
+    public User authentication(String login, String password) {
+        User user = getByEmail(login);
+        if (user == null) {return null;}
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+            messageDigest.update(password.getBytes());
+            String result = new String(messageDigest.digest());
+            if (result.equals(user.getPassword())) {
+                return user;
+            }
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public User getUserById(int id) {
+        UserDAO userDAO = UserDAO.getUserDAO();
+        return userDAO.getById(id);
+    }
+
+    public void update(User user) {
+        UserDAO userDAO = UserDAO.getUserDAO();
+        userDAO.update(user);
+    }
+
+    public List<User> getAllUsers() {
+        return UserDAO.getUserDAO().getAll();
+    }
+
+    public User getBoss(User u) {
+        if (u.getBoss() == null) {
+            return null;
+        }
+        return UserDAO.getUserDAO().getById(u.getBoss().getId());
+    }
+
+    public List<User> getEmployees(User u) {
+        return UserDAO.getUserDAO().getEmployees(u);
+    }
+
+
+}
